7df72ee2848ab04b824357a69fcda9a785bb88bb
dci-student <oleksandra.adamchyk@dci-student.org>
2025-11-13 10:13:09 +0100

    add CORS headers to allow frontend requests

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -153,7 +153,7 @@ if not DEBUG:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
